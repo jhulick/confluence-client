5d116dc933c780fd62ccb2be03c6ed0b35ce16e8
blair christensen <blair.christensen@gmail.com>
2011-04-04 15:30:00 -0500

    Added 'Confluence#add_space', 'Confluence#get_space' and 'Confluence#remove_space'

--- lib/confluence-client.rb
+++ lib/confluence-client.rb
@@ -15,6 +15,24 @@ module Confluence # :nodoc:
   #       # Print error message if error on last API call.
   #       puts confluence.error if confluence.error?
   #       
+  #       space = confluence.get_space('foo')
+  #       if space
+  #         puts "found space: #{space.inspect}"
+  #       else
+  #         space = confluence.add_space( 'foo', 'space name', 'space description' )
+  #         if space
+  #           puts "created space: #{space.inspect}"
+  #         else
+  #           puts "unable to create space: #{c.error}"
+  #         end
+  #       end
+  #       
+  #       if confluence.remove_space('foo')
+  #         puts 'removed space'
+  #       else
+  #         puts "unable to remove space: #{c.error}"
+  #       end
+  #       
   #       confluence.logout
   #     end
   #
@@ -45,9 +63,31 @@ module Confluence # :nodoc:
       yield self if block_given?
     end
 
+    # Create Confluence space.  Returns space or nil.
+    #
+    # Params:
+    # +key+:: Key for space.
+    # +name+:: Name of space.
+    # +description+:: Description for space.
+    def add_space(key, name, description)
+      space = addSpace( { 'key' => key, 'name' => name, 'description' => description } )
+      return space if ok?
+      nil 
+    end
+
     # Was there an error on the last request?
     def error?
       !ok?
+    end
+
+    # Return Confluence space or nil.
+    #
+    # Params:
+    # +key+:: Confluence key for space.
+    def get_space(key)
+      space = getSpace(key)
+      return space if ok?
+      nil
     end
 
     # Login to the Confluence XML/RPC API.
@@ -58,9 +98,9 @@ module Confluence # :nodoc:
         @token  = @confluence.login(@user, @password)
         @error  = nil
       rescue XMLRPC::FaultException => e
-        @error = e.faultString
+        @error = tidy_exception( e.faultString )
       rescue => e
-        @error = e.message
+        @error = tidy_exception( e.message )
       end
       return ok?
     end
@@ -74,9 +114,9 @@ module Confluence # :nodoc:
         @error = nil
         return @confluence.send( method_name, *( [@token] + args ) )  
       rescue XMLRPC::FaultException => e
-        @error = e.faultString
+        @error = tidy_exception( e.faultString )
       rescue Exception => e
-        @error = e.message
+        @error = tidy_exception( e.message )
       end
       return ok?
     end
@@ -84,6 +124,22 @@ module Confluence # :nodoc:
     # Was the last request successful?
     def ok?
       @error.nil?
+    end
+
+    # Remove Confluence space.  Returns boolean.
+    #
+    # Params:
+    # +key+:: Confluence key for space to remove.
+    def remove_space(key)
+      return removeSpace(key)
+    end
+
+    # Make the Confluence exceptions more readable.
+    #
+    # Params:
+    # +txt+:: Exception text to clean up.
+    def tidy_exception(txt)
+      txt.gsub!( /^java.lang.Exception: com.atlassian.confluence.rpc.RemoteException:\s+/, '' )
     end
 
   end # class Client
